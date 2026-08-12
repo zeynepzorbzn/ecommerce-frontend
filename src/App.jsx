ddbb05dde29f117df6a96ea8cae728da94e0a19a
zeynepzorbzn <zeynepnurzorbozan@gmail.com>
2026-08-12 14:28:05 +0300

    The project file was created, and React, Redux Toolkit, and Tailwind CSS were integrated.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-900">
+          E-Commerce
+        </h1>
+      </div>
+  )
+}
+
+export default App
